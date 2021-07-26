8dbfe8f1a2a8ab1cdaf6864a13221f56952c998f
max <punkimen666@gmail.com>
2021-07-26 13:15:36 +0300

    form-validate

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,6 +5,28 @@
 //     new bootstrap.Tooltip(tooltipTriggerEl)
 //   })
 // })()
+
+// Пример стартового JavaScript для отключения отправки форм при наличии недопустимых полей
+(function () {
+  'use strict'
+
+  // Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Зацикливайтесь на них и предотвращайте отправку
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})();
+
 (function () {
   'use strict'
   const loginForm = document.querySelector('.login-form')
@@ -48,3 +70,4 @@
     })
   })
 })()
+
